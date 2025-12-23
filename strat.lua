3e79c8f01f4f556e351f1173976d9d16fa070574
sigma boy <sulthon.rasyidin@gmail.com>
2025-12-23 07:28:24 +0700

    Update strat.lua

--- strat.lua
+++ strat.lua
@@ -6,7 +6,7 @@
             _G.AutoSkip = false
             _G.AutoSnowballs = true
 
-local TDS = loadstring(game:HttpGet("https://raw.githubusercontent.com/DuxiiT/tds-autostrat/refs/heads/main/main.lua"))()
+local TDS = loadstring(game:HttpGet("https://raw.githubusercontent.com/zeyseil/test124342/refs/heads/main/main.lua"))()
 
             -- Example Strat
             while _G.AutoStrat do
